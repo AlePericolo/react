e04dbaaaff339dc3c9b68f1aa56ffb940f3808b1
alepericolo <ale.pericolo@gmail.com>
2020-01-08 23:04:14 +0100

    component

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Ciao(props) {
+  return <h1>Ciao, {props.nome}</h1>;
+}
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <Ciao nome="Ale" />
+       
         <a
           className="App-link"
           href="https://reactjs.org"
